7a8ffd36a0dcd146bc4b514522807e19960d7327
Adam Binford <adamq43@gmail.com>
2024-03-24 18:22:11 -0400

    Run just EC tests and show java logs

--- crates/hdfs-native/src/minidfs.rs
+++ crates/hdfs-native/src/minidfs.rs
@@ -75,7 +75,7 @@ impl MiniDfs {
             ])
             .stdin(Stdio::piped())
             .stdout(Stdio::piped())
-            .stderr(Stdio::null())
+            // .stderr(Stdio::null())
             .spawn()
             .unwrap();
 
