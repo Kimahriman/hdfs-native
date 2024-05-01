6da97f49f8fd2c0aaa390a75078563f491bc4afa
Adam Binford <adamq43@gmail.com>
2024-04-30 21:08:50 -0400

    Fix doctest

--- crates/hdfs-native-object-store/src/lib.rs
+++ crates/hdfs-native-object-store/src/lib.rs
@@ -5,7 +5,7 @@
 //!
 //! ```rust
 //! use hdfs_native_object_store::HdfsObjectStore;
-//! # use hdfs_native::Result;
+//! # use object_store::Result;
 //! # fn main() -> Result<()> {
 //! let store = HdfsObjectStore::with_url("hdfs://localhost:9000")?;
 //! # Ok(())
