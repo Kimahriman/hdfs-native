90d49f109eb97f70808fa97db4a50957653ddd6e
Adam Binford <adamq43@gmail.com>
2024-05-01 21:59:29 -0400

    Update integration tests to use hadoop 3.4.0

--- crates/hdfs-native/tests/test_integration.rs
+++ crates/hdfs-native/tests/test_integration.rs
@@ -34,6 +34,7 @@ mod test {
 
     #[tokio::test]
     #[serial]
+    #[cfg(feature = "kerberos")]
     async fn test_integrity_kerberos() {
         test_with_features(&HashSet::from([
             DfsFeatures::Security,
@@ -81,6 +82,7 @@ mod test {
 
     #[tokio::test]
     #[serial]
+    #[cfg(feature = "kerberos")]
     async fn test_aes() {
         test_with_features(&HashSet::from([
             DfsFeatures::Security,
@@ -93,6 +95,7 @@ mod test {
 
     #[tokio::test]
     #[serial]
+    #[cfg(feature = "kerberos")]
     async fn test_forced_data_transfer_encryption() {
         // DataTransferEncryption enabled but privacy isn't, still force encryption
         test_with_features(&HashSet::from([
@@ -105,6 +108,7 @@ mod test {
 
     #[tokio::test]
     #[serial]
+    #[cfg(feature = "kerberos")]
     async fn test_data_transfer_encryption() {
         test_with_features(&HashSet::from([
             DfsFeatures::Security,
@@ -117,6 +121,7 @@ mod test {
 
     #[tokio::test]
     #[serial]
+    #[cfg(feature = "kerberos")]
     async fn test_data_transfer_encryption_aes() {
         test_with_features(&HashSet::from([
             DfsFeatures::Security,
@@ -163,6 +168,7 @@ mod test {
 
     #[tokio::test]
     #[serial]
+    #[ignore]
     async fn test_rbf() {
         test_with_features(&HashSet::from([DfsFeatures::RBF]))
             .await
