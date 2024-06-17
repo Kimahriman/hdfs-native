c39e0efa18f639113bb35da78b9b165d85225193
Adam Binford <adamq43@gmail.com>
2024-06-17 06:59:44 -0400

    Better feature flagging for bechmarks

--- rust/benches/rpc.rs
+++ rust/benches/rpc.rs
@@ -1,7 +1,6 @@
 use std::collections::HashSet;
 
 use criterion::*;
-use hdfs::hdfs::get_hdfs;
 use hdfs_native::{minidfs::MiniDfs, Client, WriteOptions};
 
 fn bench(c: &mut Criterion) {
@@ -25,14 +24,14 @@ fn bench(c: &mut Criterion) {
             .unwrap();
     });
 
-    let fs = get_hdfs().unwrap();
-
     let mut group = c.benchmark_group("rpc");
     group.bench_function("getFileInfo-native", |b| {
         b.to_async(&rt)
             .iter(|| async { client.get_file_info("/bench").await.unwrap() })
     });
+    #[cfg(unix)]
     group.bench_function("getFileInfo-libhdfs", |b| {
+        let fs = hdfs::hdfs::get_hdfs().unwrap();
         b.iter(|| fs.get_file_status("/bench").unwrap())
     });
 }
