dd63852c80f10da3bba4ec151c47cadcf7698a89
Adam Binford <adamq43@gmail.com>
2024-11-02 10:28:24 -0400

    Add more output to errors (#144)

--- rust/src/error.rs
+++ rust/src/error.rs
@@ -5,45 +5,45 @@ use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum HdfsError {
-    #[error("IO error occurred while communicating with HDFS")]
+    #[error("IO error occurred while communicating with HDFS: {0}")]
     IOError(#[from] io::Error),
-    #[error("data transfer error")]
+    #[error("data transfer error: {0}")]
     DataTransferError(String),
     #[error("checksums didn't match")]
     ChecksumError,
-    #[error("invalid path")]
+    #[error("invalid path: {0}")]
     InvalidPath(String),
-    #[error("invalid argument")]
+    #[error("invalid argument: {0}")]
     InvalidArgument(String),
-    #[error("failed to parse URL")]
+    #[error("failed to parse URL: {0}")]
     UrlParseError(#[from] url::ParseError),
-    #[error("file already exists")]
+    #[error("file already exists: {0}")]
     AlreadyExists(String),
-    #[error("operation failed")]
+    #[error("operation failed: {0}")]
     OperationFailed(String),
-    #[error("file not found")]
+    #[error("file not found: {0}")]
     FileNotFound(String),
-    #[error("blocks not found")]
+    #[error("blocks not found for {0}")]
     BlocksNotFound(String),
-    #[error("path is a directory")]
+    #[error("path is a directory: {0}")]
     IsADirectoryError(String),
-    #[error("unsupported erasure coding policy")]
+    #[error("unsupported erasure coding policy {0}")]
     UnsupportedErasureCodingPolicy(String),
-    #[error("erasure coding error")]
+    #[error("erasure coding error: {0}")]
     ErasureCodingError(String),
-    #[error("operation not supported")]
+    #[error("operation not supported: {0}")]
     UnsupportedFeature(String),
-    #[error("interal error, this shouldn't happen")]
+    #[error("interal error, this shouldn't happen: {0}")]
     InternalError(String),
-    #[error("failed to decode RPC response")]
+    #[error("failed to decode RPC response: {0}")]
     InvalidRPCResponse(#[from] DecodeError),
-    #[error("RPC error")]
+    #[error("RPC error: {0} {1}")]
     RPCError(String, String),
-    #[error("fatal RPC error")]
+    #[error("fatal RPC error: {0} {1}")]
     FatalRPCError(String, String),
-    #[error("SASL error")]
+    #[error("SASL error: {0}")]
     SASLError(String),
-    #[error("GSSAPI error")]
+    #[error("GSSAPI error: {0:?} {1} {2}")]
     GSSAPIError(crate::security::gssapi::GssMajorCodes, u32, String),
     #[error("No valid SASL mechanism found")]
     NoSASLMechanism,
