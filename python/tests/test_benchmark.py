9c46675e540cf435817aa1a475a813ff2f2a4299
Adam Binford <adamq43@gmail.com>
2024-11-05 21:29:12 -0500

    Release GIL when blocking on inner calls

---
+++ python/tests/test_benchmark.py
@@ -0,0 +1,21 @@
+from concurrent.futures import ThreadPoolExecutor
+
+from pytest_benchmark.fixture import BenchmarkFixture
+
+from hdfs_native import Client
+
+
+def do_work(client: Client):
+    def delete(path: str):
+        client.delete(path)
+
+    with ThreadPoolExecutor(100) as executor:
+        for i in range(100):
+            executor.submit(delete, f"/bench{i}")
+
+
+def test_threading(client: Client, benchmark: BenchmarkFixture):
+    for i in range(100):
+        client.create(f"/bench{i}").close()
+
+    benchmark(do_work, client)
