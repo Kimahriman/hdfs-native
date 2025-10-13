58dc829729ef5c5d9c47268ce08cdc4ba2335db4
Adam Binford <adamq43@gmail.com>
2025-10-13 13:17:20 -0400

    Bump version to 0.12.3

--- python/docs/source/conf.py
+++ python/docs/source/conf.py
@@ -9,7 +9,7 @@
 project = "hdfs-native"
 copyright = "2025, Adam Binford"
 author = "Adam Binford"
-release = "0.12.2"
+release = "0.12.3"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
